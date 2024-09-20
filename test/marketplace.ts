7281a7b1003c0eaa870e02046c9518391214dea5
koxy <devkoxy@gmail.com>
2024-09-21 00:41:10 +0100

    create offer test works fine

--- test/marketplace.ts
+++ test/marketplace.ts
@@ -67,15 +67,47 @@ describe("Marketplace", function () {
 
       expect(cancelListing).to.emit(NFTMarketplace, "NFTListingCancelled").withArgs(1, owner);
     });
-    it("A buyer should make an offer on List Nft", async function () {
+    it("A buyer should make an offer on Listed Nft", async function () {
       const { NFTMarketplace, owner } = await loadFixture(deployNftMarketplace);
       const uri = "https://lavender-electric-gerbil-466.mypinata.cloud/ipfs/Qmaz3L8dtMxR3k1B8wauqZe5riv7xUFanQxMp2AkJz3V34";
       const newNFt = await NFTMarketplace.mint(uri)
-      const newListing = await NFTMarketplace.listNFT(1, hre.ethers.parseEther("1000")); 
-      const cancelListing = await NFTMarketplace.connect(owner).cancelListing(1); 
+      const newListing = await NFTMarketplace.listNFT(1, hre.ethers.parseEther("1000"));
+
+      const [otherAccount, signer2] = await hre.ethers.getSigners();
+      
+      const makeOffer = await NFTMarketplace.connect(signer2).makeOffer(1, {value: hre.ethers.parseEther("2000")}); 
 
 
-      expect(cancelListing).to.emit(NFTMarketplace, "NFTListingCancelled").withArgs(1, owner);
+      expect(makeOffer).to.emit(NFTMarketplace, "OfferCreated").withArgs(1, signer2.address, hre.ethers.parseEther("2000"));
+    });
+
+    it("A buyer should cancel an offer on Listed Nft", async function () {
+      const { NFTMarketplace, owner } = await loadFixture(deployNftMarketplace);
+      const uri = "https://lavender-electric-gerbil-466.mypinata.cloud/ipfs/Qmaz3L8dtMxR3k1B8wauqZe5riv7xUFanQxMp2AkJz3V34";
+      const newNFt = await NFTMarketplace.mint(uri)
+      const newListing = await NFTMarketplace.listNFT(1, hre.ethers.parseEther("1000"));
+
+      const [otherAccount, signer2] = await hre.ethers.getSigners();
+      
+      const makeOffer = await NFTMarketplace.connect(signer2).makeOffer(1, {value: hre.ethers.parseEther("2000")}); 
+
+      const cancelOffer = await NFTMarketplace.connect(signer2).cancelOffer(1);
+
+
+      expect(cancelOffer).to.emit(NFTMarketplace, "OfferCreated").withArgs(1, signer2.address, hre.ethers.parseEther("2000"));
+    });
+
+    it("Owner should accept a specific offer on Listed Nft", async function () {
+      const { NFTMarketplace, owner } = await loadFixture(deployNftMarketplace);
+      const uri = "https://lavender-electric-gerbil-466.mypinata.cloud/ipfs/Qmaz3L8dtMxR3k1B8wauqZe5riv7xUFanQxMp2AkJz3V34";
+      const newNFt = await NFTMarketplace.mint(uri)
+      const newListing = await NFTMarketplace.listNFT(1, hre.ethers.parseEther("1000"));
+      const [otherAccount, signer2] = await hre.ethers.getSigners();
+      const makeOffer = await NFTMarketplace.connect(signer2).makeOffer(1, {value: hre.ethers.parseEther("2000")}); 
+      const cancelOffer = await NFTMarketplace.connect(signer2).cancelOffer(1);
+
+
+      expect(cancelOffer).to.emit(NFTMarketplace, "OfferCreated").withArgs(1, signer2.address, hre.ethers.parseEther("2000"));
     });
     
     
